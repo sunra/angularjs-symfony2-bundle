7c92c65d0d6001be6e877df3bf11fb3ddc9cdf27
Tomasz <tomasz.skorupka@goldbachaudience.pl>
2015-06-10 10:04:33 +0200

    Added clearinput

---
+++ Sunra/AngularBundle/Resources/public/js/thirdparty/angular-clear-input-master/angular.dcb-clear-input.js
@@ -0,0 +1,47 @@
+/*
+ Angular Clear Input directive
+ (c) 2014 Daniel Cohen. http://dcb.co.il
+ License: MIT
+ */
+angular.module('dcbClearInput', [])
+    .directive('clearInput', ['$parse',
+        function($parse) {
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                link: function(scope, element, attr) {
+                    var htmlMarkup = attr.clearBtnMarkup ? attr.clearBtnMarkup : '<span>X</span>';
+                    var btn = angular.element(htmlMarkup);
+                    btn.addClass(attr.clearBtnClass ? attr.clearBtnClass : "clear-btn");
+                    element.after(btn);
+
+                    btn.on('click', function(event) {
+                        if (attr.clearInput) {
+                            var fn = $parse(attr.clearInput);
+                            scope.$apply(function() {
+                                fn(scope, {
+                                    $event: event
+                                });
+                            });
+                        } else {
+                            scope[attr.ngModel] = null;
+                            scope.$digest();
+                        }
+                    });
+
+                    scope.$watch(attr.ngModel, function(val) {
+                        var hasValue = val && val.length > 0;
+                        if (!attr.clearDisableVisibility) {
+                            btn.css('visibility', hasValue ? 'visible' : 'hidden');
+                        }
+
+                        if (hasValue && !btn.hasClass('clear-visible')) {
+                            btn.removeClass('clear-hidden').addClass('clear-visible');
+                        } else if (!hasClass && !btn.hasClass('clear-hidden')) {
+                            btn.removeClass('clear-visible').addClass('clear-hidden');
+                        }
+                    });
+                }
+            };
+        }
+    ]);
